6ee025dedb02886fd3ee81055fc1557dfb84c084
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-21 08:11:39 -0300

    Adicionando método de verificação de token

---
+++ BotMessenger/src/main/java/br/com/ubots/estagio/BotMessenger/BotMessengerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ubots.estagio.BotMessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotMessengerApplication.class, args);
+	}
+
+}
---
+++ BotMessenger/src/test/java/br/com/ubots/estagio/BotMessenger/BotMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ubots.estagio.BotMessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
