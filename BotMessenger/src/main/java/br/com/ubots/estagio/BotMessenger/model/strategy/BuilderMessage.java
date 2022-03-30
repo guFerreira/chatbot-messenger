09501c4c827d038cd855b0dc193ec3da1f1e9ca8
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-30 09:57:23 -0300

    Criação de método responsável por adicionar as estratégias ao contexto

--- BotMessenger/src/main/java/br/com/ubots/estagio/BotMessenger/model/strategy/BuilderMessage.java
+++ BotMessenger/src/main/java/br/com/ubots/estagio/BotMessenger/model/strategy/BuilderMessage.java
@@ -5,25 +5,30 @@ import java.util.List;
 
 public class BuilderMessage {
     private String receivedMessage;
-    private List<Response> responses = new ArrayList<Response>();;
-    private Response callback;
+    private List<MessageCreationStrategy> strategies;
+    private MessageCreationStrategy fallbackStrategy;
 
     public BuilderMessage(String receivedMessage) {
-        this.responses.add(new Idade());
-        this.responses.add(new Nome());
-        this.responses.add(new Weather(receivedMessage));
-
-        this.callback = new Incompreendido();
         this.receivedMessage = receivedMessage;
+        this.strategies = new ArrayList<MessageCreationStrategy>();
+        this.addMessageCreationStrategiesToTheContext();
+    }
+
+    private void addMessageCreationStrategiesToTheContext(){
+        this.strategies.add(new AgeStrategy());
+        this.strategies.add(new NameStrategy());
+        this.strategies.add(new WeatherStrategy(this.receivedMessage));
+
+        this.fallbackStrategy = new FallbackStrategy();
     }
 
     public String buildMessage(){
-        for (Response response: this.responses) {
-            if(response.verifyIntents(this.receivedMessage)){
-                return response.buildMessage();
+        for (MessageCreationStrategy strategy : this.strategies) {
+            if(strategy.verifyIntents(this.receivedMessage)){
+                return strategy.buildMessage();
             }
         }
-        return this.callback.buildMessage();
+        return this.fallbackStrategy.buildMessage();
     }
 
 }
