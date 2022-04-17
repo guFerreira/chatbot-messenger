37267bcfbeacba5fd68dbdcce45ac05177e90c83
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-04-17 18:09:01 -0300

    Adicionando service que se conecta e manda as mensagens para o dialogflow

---
+++ BotMessenger/src/main/java/br/com/ubots/estagio/BotMessenger/service/AgentService.java
@@ -0,0 +1,44 @@
+package br.com.ubots.estagio.BotMessenger.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.api.gax.rpc.ApiException;
+
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.QueryResult;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import com.google.cloud.dialogflow.v2.TextInput;
+
+public class AgentService {
+    private String languageCode = "pt-BR";
+
+    private String projectId = "chatbottest-ewkm";
+
+    public QueryResult detectIntentTexts(String text, String sessionId) {
+        try (SessionsClient sessionsClient = SessionsClient.create()) {
+            SessionName session = SessionName.of(projectId, sessionId);
+
+            DetectIntentResponse response = sessionsClient
+                    .detectIntent(session, this.buildQueryInput(text));
+
+            return response.getQueryResult();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private TextInput buildTextInput(String text) {
+        return TextInput.newBuilder().setText(text).setLanguageCode(languageCode).build();
+    }
+
+    private QueryInput buildQueryInput(String text) {
+        return QueryInput.newBuilder()
+                .setText(this.buildTextInput(text))
+                .build();
+    }
+}
